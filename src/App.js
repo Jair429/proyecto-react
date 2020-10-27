ce7e70ea9e9e2df06f865040b7b9280c11436203
Jair <jair.terrones429@gmail.com>
2020-10-27 00:21:39 -0500

    primer commit del proyecto React.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { Fragment } from "react";
+import "./styles/styles.scss";
+import Curso from "./Curso";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <>
+    <div class="main-banner img-container l-section" id="main-banner">
+      <div class="ed-grid lg-grid-6">
+        <div class="lg-cols-4 lg-x-2">
+          <img
+            class="main-banner__img"
+            src="https://images.pexels.com/photos/1643383/pexels-photo-1643383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            alt="banner"
+          />
+          <div class="main-banner__data s-center">
+            <p class="t2 s-mb-0">Título del banner</p>
+            <p> Subtítulo del banner</p>
+            <a href="https://ed.team" class="button">
+              Suscribirse
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
-  );
-}
+
+    <div className="ed-grid m-grid-3">
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+    </div>
+  </>
+);
 
 export default App;
+
+// Reglas JSX
+// 1: Toda etiqueta debe cerrarse
+// 2: Los componentes deben devolver UN sólo elemento padre
+// 3: Apoyarse de los Fragment cuando necesito devolver 2 elementos
+// 4: Fragment => <> hijos </>
+// 5: img siempre se cierra
+// 6: class => className
+// 7: for => htmlFor
---
+++ src/Curso.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Curso = () => (
+  <article classNameName="card">
+    <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
+      <img src="https://ux.ed.team/img/curso-portada.png" alt="Go" />
+    </div>
+    <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
+      <h3 className="t5 s-mb-2 s-center">
+        Programación orientada a objetos con Go
+      </h3>
+      <div className="s-mb-2 s-main-center">
+        <div className="card__teacher s-cross-center">
+          <div className="card__avatar s-mr-1">
+            <div className="circle img-container">
+              <img src="https://ux.ed.team/img/profesor.jpg" alt="profe" />
+            </div>
+          </div>
+          <span className="small">Alexys Lozada</span>
+        </div>
+      </div>
+      <div className="s-main-center">
+        <a
+          className="button--ghost-alert button--tiny"
+          href="https://google.com"
+        >
+          $ 20USD
+        </a>
+      </div>
+    </div>
+  </article>
+);
+
+export default Curso;
